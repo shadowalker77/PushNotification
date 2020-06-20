51bd39ab5326b4d143f0cf87c602956e699819eb
A.Badakhshan <ugdvqh123>
2020-06-20 11:51:10 +0430

    0.4.4

--- app/src/main/java/ir/ayantech/pushnotification/MainActivity.kt
+++ app/src/main/java/ir/ayantech/pushnotification/MainActivity.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.widget.Toast
 import ir.ayantech.pushsdk.core.AyanNotification
 import ir.ayantech.pushsdk.helper.ShareHelper
+import ir.ayantech.pushsdk.model.MessageDeserializer
 import ir.ayantech.pushsdk.model.api.PushNotification
 import ir.ayantech.pushsdk.storage.PushNotificationUser
 import kotlinx.android.synthetic.main.activity_main.*
 import me.yokeyword.fragmentation.SupportActivity
+import java.lang.Exception
 
 class MainActivity : SupportActivity() {
 
@@ -23,6 +25,16 @@ class MainActivity : SupportActivity() {
         copyBtn.setOnClickListener {
             ShareHelper.copyToClipBoard(this, PushNotificationUser.getPushNotificationToken())
             Toast.makeText(this, "Copied", Toast.LENGTH_LONG).show()
+        }
+
+        testMessageBtn.setOnClickListener {
+            try {
+                val message = MessageDeserializer.stringToMessage("{\"actionType\":\"Share\",\"action\":{\"model\":{\"content\":\"share text\"}},\"notificationToShow\":{\"buttons\":[{\"message\":{\"action\":{\"model\":{}},\"actionType\":\"NoAction\"},\"text\":\"close\"}],\"imageUrl\":null,\"message\":\"\",\"title\":\"salam test\"}}", "")
+                AyanNotification.performMessageLogic(this, message)
+                Log.d("Msg", message.toString())
+            } catch (e: Exception) {
+                Log.e("Msg", e.message)
+            }
         }
 
         Log.d("TOKEN", PushNotificationUser.getPushNotificationToken())
